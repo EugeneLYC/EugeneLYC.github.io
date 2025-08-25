259d9c0a47cbc6f61e474e69299bd75b74cbb49d
EugeneLYC <randylyc@outlook.com>
2025-08-25 14:13:21 +0800

    Update job.

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-python jemdoc.py -c configs/pub.conf publications.jemdoc
-python jemdoc.py -c configs/mysite.conf index.jemdoc
-python jemdoc.py -c configs/lab.conf lab.jemdoc
+python3 jemdoc.py -c configs/pub.conf publications.jemdoc
+python3 jemdoc.py -c configs/mysite.conf index.jemdoc
+python3 jemdoc.py -c configs/lab.conf lab.jemdoc

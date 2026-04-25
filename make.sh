91ebc4b58db50e0a60d915d295b78484775f38b9
EugeneLYC <randylyc@outlook.com>
2026-04-25 19:18:50 +0800

    Update group info.

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 python3 jemdoc.py -c configs/pub.conf publications.jemdoc
 python3 jemdoc.py -c configs/mysite.conf index.jemdoc
 python3 jemdoc.py -c configs/lab.conf lab.jemdoc
+python3 jemdoc.py -c configs/joinus.conf joinus.jemdoc
